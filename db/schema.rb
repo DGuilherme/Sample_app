d18d227e7fc95118e1842c7ae64625a4727f86f7
DGuilherme <th3vid98@gmail.com>
2019-03-05 18:16:09 +0000

    Correçao de erros addiçao do sendgrid

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190304164934) do
+ActiveRecord::Schema.define(version: 20190305181016) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
@@ -32,8 +31,6 @@ ActiveRecord::Schema.define(version: 20190304164934) do
     t.string "activation_digest"
     t.boolean "activated"
     t.datetime "activated_at"
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
