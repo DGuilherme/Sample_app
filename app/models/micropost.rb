896a7c71ef94f09e03b18a5208e4b0050c628835
DGuilherme <th3vid98@gmail.com>
2019-03-06 09:02:12 +0000

    test resolution

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,12 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
-  default_scope -> { order(created_at: :desc)}
-  validates :user_id, presence: true
-  validates :content, presence: true, length: {maximum: 140}
-=======
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
->>>>>>> f9bea67d04cef1c6b899876c439cbc707f5fef61
 end
